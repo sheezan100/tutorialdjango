0538a8abde3e6363dfa74cec3a81d9df5004a599
sheezan <sheezan000.sheez@gmail.com>
2022-12-13 12:25:22 +0530

    Added  new line forms.py

--- polls/forms.py
+++ polls/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class LoginForm(forms.Form):
    user = forms.CharField(max_length = 100)
    password = forms.CharField(widget = forms.PasswordInput())
+   password = forms.CharField(widget=forms.PasswordInput())
